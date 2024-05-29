5ed46368344168ffd0f2af176e55907d80b37e2e
qkinggg <qkinggg@gmail.com>
2024-05-29 16:55:10 +0800

    0.1.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,40 @@
- 
+ # app/__init__.py
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+mongo = PyMongo()   # Flask擴展，用於將Flask應用與MongoDB數據庫集成。
+bcrypt = Bcrypt()   # Flask擴展，用於加密和驗證密碼
+jwt = JWTManager()  # Flask擴展，用於處理基於JSON Web Tokens (JWT) 的身份驗證。
+
+def create_app():
+    app = Flask(__name__)
+
+    load_dotenv()
+    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    # 初始化擴展
+    mongo.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    @app.route('/')
+    def index():
+        return 'Hello, welcome to the E-commerce platform!'
+    '''
+    # 註冊藍圖（在後續步驟中創建）
+    from .routes.auth import auth_bp
+    from .routes.product import product_bp
+    from .routes.cart import cart_bp
+    from .routes.order import order_bp
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(product_bp, url_prefix='/product')
+    app.register_blueprint(cart_bp, url_prefix='/cart')
+    app.register_blueprint(order_bp, url_prefix='/order')
+    '''
+    return app
--- app/models.py
+++ app/models.py
@@ -1,1 +1,35 @@
- 
+ # app/models.py
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+class User:
+    def __init__(self, username, email, password, role='user'):
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.role = role
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Product:
+    def __init__(self, name, description, price, stock, image_url):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stock = stock
+        self.image_url = image_url
+
+class Cart:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.products = []
+
+class Order:
+    def __init__(self, user_id, products, total_price, status='processing'):
+        self.user_id = user_id
+        self.products = products
+        self.total_price = total_price
+        self.status = status
+        self.created_at = datetime.now()
+        
